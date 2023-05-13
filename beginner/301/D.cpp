e027f4ae83762ccbeee8019df71f736b8fc2acce
afuku08 <fukkuseiya@gmail.com>
2023-05-13 22:17:36 +0900

    AtCoder301

---
+++ beginner/301/D.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    long long n;
+    cin >> n;
+    int tmp = 0;
+    for(int i=0;i<s.length();i++){
+        if()
+    }
+}
