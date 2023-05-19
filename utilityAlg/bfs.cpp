0ff275fe6e997345a2e0babf33f36f1cbaf485f9
afuku08 <fukkuseiya@gmail.com>
2023-05-19 16:37:21 +0900

    add bfs

---
+++ utilityAlg/bfs.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+vector<int> BFS(const Graph &G, int s){
+    int N = G.size();
+    vector<int> dist(N, -1);
+    queue<int> que;
+
+    dist[s] = 0;
+    que.push(s);
+
+    while(!que.empty()) {
+        int v = que.front();
+        que.pop();
+
+        for(int x : G[v]){
+            if(dist[x] != -1) continue;
+
+            dist[x] = dist[v] + 1;
+            que.push(x);
+        }
+    }
+    return dist;
+}
+
+int main(){
+    int N, M, s, t;
+    cin >> N >> M >> s >> t;
+
+    Graph G(N);
+    for(int i=0;i<M;++i){
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+    }
+
+    vector<int> ans = BFS(G, s);
+    if(ans[t] != -1) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
